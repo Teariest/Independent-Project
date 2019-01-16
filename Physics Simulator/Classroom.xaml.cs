416392d4b963aa2537c41292ab5975bc8ed2f10d
Clement Faisandier <s-faisandierc@bsd405.org>
2019-01-16 01:48:50 -0800

    Getting the Lessons done, the 1st one is getting its structure and style finished, only hurdle is going to be the simulation

--- Physics Simulator/Classroom.xaml.cs
+++ Physics Simulator/Classroom.xaml.cs
@@ -21,16 +21,41 @@ namespace Physics_Simulator {
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class Classroom : Page {
+
+        private int PrevSelect = 1;
+
         public Classroom() {
             this.InitializeComponent();
+
+            LessonFrame.Navigate(typeof(Lesson1));
+            MenuItem1.IsSelected = true;
         }
 
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+            bool reset = false;
             
             if (MenuHamburgerItem.IsSelected) {
                 MenuSplitView.IsPaneOpen = !MenuSplitView.IsPaneOpen;
                 MenuHamburgerItem.IsSelected = false;
+                reset = true;
             }
+
+            if (MenuItem1.IsSelected || (reset && PrevSelect == 1)) {
+                LessonFrame.Navigate(typeof(Lesson1));
+                PrevSelect = 1;
+            }
+
+            /*
+            if (MenuItem1.IsSelected || (reset && PrevSelect == 2)) {
+                LessonFrame.Navigate(typeof(Lesson2));
+                PrevSelect = 2;
+            }
+
+            if (MenuItem2.IsSelected || (reset && PrevSelect == 3)) {
+                LessonFrame.Navigate(typeof(Lesson3));
+                PrevSelect = 3;
+            }*/
         }
     }
 }
